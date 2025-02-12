32ffce041e3e9aa4ab34e393ffd5135bae70c495
Vignesh-Poonja <146766203+Vignesh-Poonja@users.noreply.github.com>
2025-02-12 11:56:29 +0000

    completed

--- Day 13/boto.py
+++ Day 13/boto.py
@@ -1,7 +1,8 @@
 import boto3
 client = boto3.client('s3')
 
-esponse = client.create_bucket(
-    Bucket='first bucket'
+response = client.create_bucket(
+    Bucket='vk-demo-boto3-123',
 )
 
+print(response)
